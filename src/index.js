5b1f68a29db271480c7fb15d00b62f40674a990a
Szymon Cendecki <31997216+SzymekCendecki@users.noreply.github.com>
2024-04-02 18:39:23 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,37 +3,52 @@ import "./style.scss";
 document.addEventListener("DOMContentLoaded", ()=>{ 
     console.log("DOM fully loaded and parsed!")
     
-    let mainDiv = document.querySelector("#main");
-    let homeClassAttr = document.querySelector("#home").getAttribute("class");
-    let gamesClassAttr = document.querySelector("#games").getAttribute("class");
-    let programsClassAttr = document.querySelector("#programs").getAttribute("class");
-    let othersClassAttr = document.querySelector("#others").getAttribute("class");
-
-    console.log(document.querySelector("#home").getAttribute("class"))
+    let home = document.querySelector("#home");
+    let games = document.querySelector("#games");
+    let programs = document.querySelector("#programs");
+    let others = document.querySelector("#others");
 
     document.querySelector("#homeBtn").addEventListener("click", ()=>{
-        document.querySelector("#home").classList.remove(document.querySelector("#home").getAttribute("class")); 
-        document.querySelector("#games").classList.remove(document.querySelector("#games").getAttribute("class"));        
-        document.querySelector("#home").classList.add("green");
-        document.querySelector("#games").classList.add("hide");    
+        home.classList.remove(home.getAttribute("class")); 
+        games.classList.remove(games.getAttribute("class"));  
+        programs.classList.remove(programs.getAttribute("class")); 
+        others.classList.remove(others.getAttribute("class"));       
+        home.classList.add("green");
+        games.classList.add("hide"); 
+        programs.classList.add("hide"); 
+        others.classList.add("hide");    
     })
 
     document.querySelector("#gamesBtn").addEventListener("click", ()=>{
-        document.querySelector("#home").classList.remove(document.querySelector("#home").getAttribute("class")); 
-        document.querySelector("#games").classList.remove(document.querySelector("#games").getAttribute("class"));
-        document.querySelector("#home").classList.add("hide");
-        document.querySelector("#games").classList.add("blue");
+        home.classList.remove(home.getAttribute("class")); 
+        games.classList.remove(games.getAttribute("class"));
+        programs.classList.remove(programs.getAttribute("class")); 
+        others.classList.remove(others.getAttribute("class")); 
+        home.classList.add("hide");
+        games.classList.add("blue");
+        programs.classList.add("hide"); 
+        others.classList.add("hide");
     })
 
     document.querySelector("#programsBtn").addEventListener("click", ()=>{
-    
-        // newDiv.classList.remove(newDiv.querySelector("class"));
-        // newDiv.classList.add("orange");    
+        home.classList.remove(home.getAttribute("class")); 
+        games.classList.remove(games.getAttribute("class"));
+        programs.classList.remove(programs.getAttribute("class")); 
+        others.classList.remove(others.getAttribute("class")); 
+        home.classList.add("hide");
+        games.classList.add("hide");
+        programs.classList.add("orange"); 
+        others.classList.add("hide"); 
     })
 
     document.querySelector("#othersBtn").addEventListener("click", ()=>{
-       
-        // newDiv.classList.remove(newDiv.querySelector("class"));
-        // newDiv.classList.add("yellow");  
+        home.classList.remove(home.getAttribute("class")); 
+        games.classList.remove(games.getAttribute("class"));
+        programs.classList.remove(programs.getAttribute("class")); 
+        others.classList.remove(others.getAttribute("class")); 
+        home.classList.add("hide");
+        games.classList.add("hide");
+        programs.classList.add("hide"); 
+        others.classList.add("yellow");
     })
 })
